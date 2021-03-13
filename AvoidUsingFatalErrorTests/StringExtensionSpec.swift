d77212e1cde49cfacb2b8baee7699d5e8be873d6
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2021-03-13 10:28:39 +0900

    REF StringExtensionsの正常パターンのテストケースを追加

---
+++ AvoidUsingFatalErrorTests/StringExtensionSpec.swift
@@ -0,0 +1,26 @@
+//
+//  StringExtensionSpec.swift
+//  AvoidUsingFatalErrorTests
+//
+//  Created by zrn_ns on 2021/03/13.
+//
+
+import Nimble
+import Quick
+
+@testable import AvoidUsingFatalError
+
+class StringExtensionSpec: QuickSpec {
+    override func spec() {
+        describe("repeated(times:)のテスト") {
+            describe("正常パターン") {
+                it("繰り返し回数が0回の場合、空文字列が返ること") {
+                    expect("abc".repeated(times: 0)) == ""
+                }
+                it("繰り返し回数が1回以上の場合、指定回数繰り返された文字列が返ること") {
+                    expect("abc".repeated(times: 3)) == "abcabcabc"
+                }
+            }
+        }
+    }
+}
